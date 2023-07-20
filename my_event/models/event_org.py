5948f7b3fb763df73ce5c1dc3c1ff2bbf80ff6f7
Ritika Shekhawat <rish@odoo.com>
2023-07-20 18:30:07 +0530

    add new feature

--- my_event/models/event_org.py
+++ my_event/models/event_org.py
@@ -2,13 +2,12 @@ from odoo import models, fields
 
 class event_org(models.Model):
     _name = "event.org"
-    _description = "Event Model"
+    _description = "Make your Event"
 
     name = fields.Char("Event Name")
     date=fields.Date("Date")
     budget = fields.Float("Your Budget")
-    venue= fields.Char("Venue")
-    # custmer=fields.Many2one('res.partner', string="Custmoer")
+    venue= fields.Selection([('G','Garden'),('H','Hall'),('Hotel','Hotel'),('re','Resturent'),('cafe','Cafe')])
     attendees=fields.Integer("Number of Attendees")
-
-
+    event_type_id=fields.Many2one("event.type",string="Event-Type")
+    # service_id=fields.One2many('event.services', 'event_id')
